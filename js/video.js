6c933194ce5b229f2be2da525adbaa0792706e83
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-03-19 11:58:34 -0500

    added attribute change

--- js/video.js
+++ js/video.js
@@ -114,6 +114,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+    $(window).on('resize load pagebeforecreate', function () {
+        if ($(window).width() < 768) {
+            $(".vid-webm").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir.webm")
+            $(".vid-ogg").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir.ogg")
+            $(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir.mp4")
+        }
+    });
+
 
 
     /*prevent scrolling on spacebar keydonw 
