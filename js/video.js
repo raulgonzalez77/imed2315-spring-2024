a38307e014368566b4a1eb6edf638bb636e01b49
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2024-03-25 15:15:59 -0500

    updates

--- js/video.js
+++ js/video.js
@@ -182,6 +182,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     //credit to https://stackoverflow.com/questions/27934728/video-doesnt-update-after-modifying-the-src-property-of-its-source-element
+    //credit to https://stackoverflow.com/questions/3983036/how-do-you-change-video-src-using-jquery
     $(window).on('resize load pagebeforecreate', function () {
         //$(document).on("load resize pagebeforecreate", function () {
         if ($(window).width() < 768) {
@@ -189,13 +190,13 @@ document.addEventListener('DOMContentLoaded', function () {
              $(".vid-ogg").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir.ogg")
              $(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir.mp4")
             */
-            //$(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir")
+            $(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir")
             //document.querySelector('.vid-mp4').src = "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_9:16/sample-wedding-video_o7wkir";
             console.log("mobile");
-            $('.BlinnVid-desktop').hide();
-            $('.BlinnVid-desktop').trigger('pause');
-            $('.BlinnVid-mobile').show();
-            $('.BlinnVid-mobile').trigger('play');
+            /* $('.BlinnVid-desktop').hide();
+             $('.BlinnVid-desktop').trigger('pause');
+             $('.BlinnVid-mobile').show();
+             $('.BlinnVid-mobile').trigger('play');*/
 
         }
         else {
@@ -203,13 +204,13 @@ document.addEventListener('DOMContentLoaded', function () {
             $(".vid-ogg").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_16:9/sample-wedding-video_o7wkir.ogg")
             $(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_16:9/sample-wedding-video_o7wkir.mp4")
             */
-            //$(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_16:9/sample-wedding-video_o7wkir")
+            $(".vid-mp4").attr("src", "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_16:9/sample-wedding-video_o7wkir")
             //document.querySelector('.vid-mp4').src = "https://res.cloudinary.com/dexnm5cpk/video/upload/f_auto:video,q_auto,c_crop,ar_16:9/sample-wedding-video_o7wkir";
             console.log('desktop');
-            $('.BlinnVid-mobile').hide();
+            /*$('.BlinnVid-mobile').hide();
             $('.BlinnVid-mobile').trigger('pause');
             $('.BlinnVid-desktop').show();
-            $('.BlinnVid-desktop').trigger('play');
+            $('.BlinnVid-desktop').trigger('play');*/
 
         }
     });
